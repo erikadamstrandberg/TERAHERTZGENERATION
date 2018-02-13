cf7b2cd08bed507423e76f7c5dd3cabf06e33328
kaztal <per@peredwardsson.se>
2018-02-13 20:32:26 +0100

    Add files via upload

--- jtestable.py
+++ jtestable.py
@@ -11,28 +11,47 @@ def main():
     maxlength = 500
     dim = [maxtime, maxlength]
     J = np.zeros(dim, dtype = complex)
-    E = np.ones(dim)/10
-    n = np.ones(dim)/10
-    v = 1/10
+    E = np.ones(dim)
+    n = np.ones(dim)
+    v = -1/10
     dt = 1
     dz = 1
     pulselength = 10
     renderstart = 0
     renderstop = 10
+    j0 = 1
+    
+    def jtestc(c, k, t):
+        return -k/c + j0*np.exp(c*t)
+    def jtest(k, t):
+        return np.exp(k*t)
 
+    t = np.arange(500)
+
+    c = v*10
+    k = 1
+
+    Jtest = np.zeros(maxtime)
+    Jtestc = np.zeros(maxtime)
+    
+    for time in t:
+        Jtest[t] = jtest(c, t)
+        Jtestc[t] = jtestc(c, k, t)
+    v = -v
     J[0, 0] = 1
     for t in range(1, J.shape[0]-1):
-        if t < pulselength:
-            J[t, 0] = 1
+        # if t < pulselength:
+        #     J[t, 0] = 1
         for z in range(J.shape[1]):
             J[t, z] = (1 - v*dt/2)/(1 + v*dt/2)*J[t-1, z] + dt/2*(n[t-1, z] + n[t, z])*E[t-1, z]/(1 + v*dt/2)
-
     mplot.figure(1)
-
+    
     # mplot.subplot(211)
     mplot.plot(J[:, 0], 'b')
-    mplot.ylim(-0.1, 1)
-    mplot.xlim(0, J.shape[1]/5)
+    mplot.plot(Jtest, 'r--')
+    mplot.plot(Jtestc, 'g--')
+    mplot.ylim(-0.1*10, 10)
+    mplot.xlim(0, 100)
     mplot.grid(True)
     mplot.xlabel('$\\frac{t}{\delta t}$')
     mplot.ylabel('$J(t)$')
