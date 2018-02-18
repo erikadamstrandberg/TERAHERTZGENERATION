689dcca8c31b9f4ecf29658d86f5a8ec6a1cdaa4
erikadamstrandberg <erikadamstrandberg@gmail.com>
2018-02-18 15:53:18 +0100

    Update Laser.py

--- Laser.py
+++ Laser.py
@@ -2,48 +2,29 @@
 """ Optional are in order amplitude, the mean and the variance of the gaussian envelope    """
 
 import numpy as np
-def Laser_gauss_cos(SIZE,OMEGA,*OPTIONS):
-    El = np.zeros(SIZE)
-    if not OPTIONS:
-        for i in range(len(El)):
-            El[i] = np.cos(OMEGA*i)*np.exp(-(i-SIZE/2)**2/1e4)
-    elif len(OPTIONS) == 1:
-        AMPLITUDE = OPTIONS[0]
-        for i in range(len(El)):
-            El[i] = AMPLITUDE*np.cos(OMEGA*i)*np.exp(-(i-SIZE/2)**2/1e4)
-    elif len(OPTIONS) == 2:
-        AMPLITUDE = OPTIONS[0]
-        MEAN = OPTIONS[1]
-        for i in range(len(El)):
-            El[i] = AMPLITUDE*np.cos(OMEGA*i)*np.exp(-(i-MEAN)**2/1e4)
-    elif len(OPTIONS) == 3:
-        AMPLITUDE = OPTIONS[0]
-        MEAN = OPTIONS[1]
-        VAR = OPTIONS[2]
-        for i in range(len(El)):
-            El[i] = AMPLITUDE*np.cos(OMEGA*i)*np.exp(-(i-MEAN)**2/VAR)
-    else:
-        for i in range(len(El)):
-            El[i] = np.cos(OMEGA*i)*np.exp(-(i-SIZE/2)**2/1e4)
-
-    return El
     
-def Laser_gauss_sin(SIZE,OMEGA,*OPTIONS):
-    El = np.zeros(SIZE)
-    if not OPTIONS:
-        for i in range(len(El)):
-            El[i] = np.sin(OMEGA*i)*np.exp(-(i-SIZE/2)**2/1e4)
-    elif len(OPTIONS) == 1:
-        MEAN = OPTIONS[0]
-        for i in range(len(El)):
-            El[i] = np.sin(OMEGA*i)*np.exp(-(i-MEAN)**2/1e4)
-    elif len(OPTIONS) == 2:
-        MEAN = OPTIONS[0]
-        VAR = OPTIONS[1]
-        for i in range(len(El)):
-            El[i] = np.sin(OMEGA*i)*np.exp(-(i-MEAN)**2/VAR)
-    else:
-        for i in range(len(El)):
-            El[i] = np.sin(OMEGA*i)*np.exp(-(i-SIZE/2)**2/1e4)
-
-    return El
+def Gauss_forward(E,B,PULSELENGTH,PULSESTART,OMEGAPRIM,VAR,dt):
+    El = np.zeros(PULSELENGTH)
+    Bl = np.zeros(PULSELENGTH)
+    START = -PULSELENGTH/2
+    STOPP = PULSELENGTH/2
+    t = dt*np.arange(START,STOPP,1)
+    for i in range(len(El)):
+        El[i] = np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*VAR)))/np.sqrt(2*np.pi*VAR)
+    for i in range(len(Bl)-1):
+        Bl[i] = (1/2)*(np.cos(OMEGAPRIM*t[i+1])*np.exp(-(t[i+1]**2/(2*VAR)))+np.cos(OMEGAPRIM*t[i+1])*np.exp(-(t[i-1]**2/(2*VAR))))/np.sqrt(2*np.pi*VAR)
+    E[PULSESTART:PULSELENGTH] = El
+    B[PULSESTART+1:PULSELENGTH+1] = Bl 
+    
+def Gauss_backward(E,B,PULSELENGTH,PULSESTART,OMEGAPRIM,VAR,dt):
+    El = np.zeros(PULSELENGTH)
+    Bl = np.zeros(PULSELENGTH)
+    START = -PULSELENGTH/2
+    STOPP = PULSELENGTH/2
+    t = dt*np.arange(START,STOPP,1)
+    for i in range(len(El)):
+        El[i] = np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*VAR)))/np.sqrt(2*np.pi*VAR)
+    for i in range(len(Bl)-1):
+        Bl[i] = -(1/2)*(np.cos(OMEGAPRIM*t[i+1])*np.exp(-(t[i+1]**2/(2*VAR)))+np.cos(OMEGAPRIM*t[i+1])*np.exp(-(t[i-1]**2/(2*VAR))))/np.sqrt(2*np.pi*VAR)
+    E[PULSESTART:PULSELENGTH] = El
+    B[PULSESTART:PULSELENGTH] = Bl 
