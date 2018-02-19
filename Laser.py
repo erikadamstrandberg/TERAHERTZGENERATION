e2c7b0afe4336d4faa6bb406764ca5ec527d4206
erikadamstrandberg <erikadamstrandberg@gmail.com>
2018-02-19 13:03:39 +0100

    Update Laser.py

--- Laser.py
+++ Laser.py
@@ -3,28 +3,28 @@
 
 import numpy as np
     
-def Gauss_forward(E,B,PULSELENGTH,PULSESTART,OMEGAPRIM,VAR,dt):
+def Gauss_forward(E,B,E0,PULSELENGTH,PULSESTART,OMEGAPRIM,VAR,dt):
     El = np.zeros(PULSELENGTH)
     Bl = np.zeros(PULSELENGTH)
     START = -PULSELENGTH/2
     STOPP = PULSELENGTH/2
     t = dt*np.arange(START,STOPP,1)
     for i in range(len(El)):
-        El[i] = np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*VAR)))/np.sqrt(2*np.pi*VAR)
-    for i in range(len(Bl)-1):
-        Bl[i] = (1/2)*(np.cos(OMEGAPRIM*t[i+1])*np.exp(-(t[i+1]**2/(2*VAR)))+np.cos(OMEGAPRIM*t[i+1])*np.exp(-(t[i-1]**2/(2*VAR))))/np.sqrt(2*np.pi*VAR)
-    E[PULSESTART:PULSELENGTH] = El
-    B[PULSESTART+1:PULSELENGTH+1] = Bl 
+        El[i] = E0*np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*VAR)))/np.sqrt(2*np.pi*VAR)
+    for i in range(len(Bl)):
+        Bl[i] = E0*(np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*VAR))))/np.sqrt(2*np.pi*VAR)
+    E[PULSESTART:PULSESTART+PULSELENGTH] = El
+    B[PULSESTART:PULSESTART+PULSELENGTH] = Bl
     
-def Gauss_backward(E,B,PULSELENGTH,PULSESTART,OMEGAPRIM,VAR,dt):
+def Gauss_backward(E,B,E0,PULSELENGTH,PULSESTART,OMEGAPRIM,VAR,dt):
     El = np.zeros(PULSELENGTH)
     Bl = np.zeros(PULSELENGTH)
     START = -PULSELENGTH/2
     STOPP = PULSELENGTH/2
     t = dt*np.arange(START,STOPP,1)
     for i in range(len(El)):
-        El[i] = np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*VAR)))/np.sqrt(2*np.pi*VAR)
-    for i in range(len(Bl)-1):
-        Bl[i] = -(1/2)*(np.cos(OMEGAPRIM*t[i+1])*np.exp(-(t[i+1]**2/(2*VAR)))+np.cos(OMEGAPRIM*t[i+1])*np.exp(-(t[i-1]**2/(2*VAR))))/np.sqrt(2*np.pi*VAR)
-    E[PULSESTART:PULSELENGTH] = El
-    B[PULSESTART:PULSELENGTH] = Bl 
+        El[i] = E0*np.cos(OMEGAPRIM*t[i-1])*np.exp(-(t[i-1]**2/(2*VAR)))/np.sqrt(2*np.pi*VAR)
+    for i in range(len(Bl)):
+        Bl[i] = -E0*(np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*VAR))))/np.sqrt(2*np.pi*VAR)
+    E[PULSESTART:PULSESTART+PULSELENGTH] = El
+    B[PULSESTART:PULSESTART+PULSELENGTH] = Bl
