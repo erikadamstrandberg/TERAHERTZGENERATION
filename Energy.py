450e06ea85237511f4d3df2dcd565bfd4dce5cc0
erikadamstrandberg <erikadamstrandberg@gmail.com>
2018-02-23 16:23:13 +0100

    Update Energy.py

--- Energy.py
+++ Energy.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 22 15:59:03 2018
-
-@author: Hanna
-"""
 import scipy.constants as const
 import numpy as np
 
@@ -13,3 +7,19 @@ q_e = const.elementary_charge
 def Ekin(J,ne):
     Ekin = (m_e/2)*np.abs(J/(ne*q_e))**2
     return Ekin
+
+def Pflux(Espace,Bspace):
+    Efs = np.fft.fft(Espace)
+    Bfs = np.fft.fft(Bspace)
+    Bfstak = np.conj(Bfs)
+    
+    Sint = np.multiply(Efs,Bfstak)
+    Scomplex = (1/2)*sum(Sint)
+    Sreal = np.real(Scomplex)
+    return Sreal
+
+def EMenergy(E,B):
+    W_e = (1/2)*np.sum(E**2)
+    W_m = (1/2)*np.sum(B**2)
+    W_em = W_e+W_m
+    return W_em
