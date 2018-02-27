ab55852e72c620dc8391d643ac3a8a289a5a73bf
erikadamstrandberg <erikadamstrandberg@gmail.com>
2018-02-27 15:12:40 +0100

    Update Fullpropagation1D.py

--- Fullpropagation1D.py
+++ Fullpropagation1D.py
@@ -13,103 +13,94 @@ from datetime import datetime
 #%% Full propagation! With W1 and W2
 def main():    
 
-    # Define the length and size of the simulation window. Units are in dt and dz respectively.
-    TIME = 400
-    SIZE = 2000
+    TIME = 1000
+    SIZE = 4000
+    plott = np.arange(TIME)
+    plotz = np.arange(SIZE)
     dim = [TIME,SIZE]
     
-    # Initialise the various fields to be calculated
     E = np.zeros(SIZE)
     Etemp = np.zeros(SIZE)
     B = np.zeros(SIZE)
     J = np.zeros(SIZE)
     n = np.zeros(SIZE)
-    ntemp = np.zeros(SIZE)
     W1 = np.zeros(SIZE)
     W2 = np.zeros(SIZE)
-
+    W3 = np.zeros(SIZE)
+    ntemp = np.zeros(SIZE)
+    Ni2 = np.zeros(SIZE)
+    Ni2temp = np.zeros(SIZE)
     Ni1 = np.zeros(SIZE)
-    Ni1tot = np.zeros(dim)
     Ni1temp = np.zeros(SIZE)
     Ni0 = np.ones(SIZE)
-    Ni0tot = np.zeros(dim)
     Ni0temp = np.zeros(SIZE)
-
     ne = np.zeros(SIZE)
     netemp = np.zeros(SIZE)
-
+    Ni0tot = np.zeros(dim)
+    Ni1tot = np.zeros(dim)
+    netot = np.zeros(dim)
     Etot = np.zeros(dim)
     Btot = np.zeros(dim)
     Jtot = np.zeros(dim)
     ntot = np.zeros(dim)
     W1tot = np.zeros(dim)
-    netot = np.zeros(dim)
-    plott = np.arange(TIME)
-    plotz = np.arange(SIZE)
-    
-    dt = 0.1        # Time step
-    dz = 0.1        # Spatial step. dt = dz is the magic step using plasma units
-    nu = 0           # Collision rate
 
-    # Parameters for laser pulse.
+    dt = 0.1
+    dz = 0.1
+    nu = 0
+
     c = const.speed_of_light
     epsilon = const.epsilon_0 
     LAMBDA = 800e-9 
     f = c/LAMBDA
     PULSELENGTH = 1000
-    PULSESTART = 0
+    PULSESTART = 1000
     OMEGAREAL = 2*np.pi*f
-    OMEGAPRIM = 2                       # this is the plasma omega, use this everywhere in the code
+    OMEGAPRIM = 1                       # this is the plasma omega, use this everywhere in the code
     OMEGA_0 = OMEGAREAL/OMEGAPRIM       # this is the arbitrary omega, use this as argument in punits
-    t0REAL = 50e-15 
+    t0REAL = 50e-16 
     I0 = 4e18 
+    NatREAL = 7e26
     E0REAL = np.sqrt(2*I0/(epsilon*c))
+
     E0 = punit.Eplasma(E0REAL,OMEGA_0)
     t0 = punit.tplasma(t0REAL,OMEGA_0)
     Laser.Gauss_forward(E,B,E0,PULSELENGTH,PULSESTART,OMEGAPRIM,t0,dt)
-    
-    # Parameters for the atomdensity
-    NatREAL = 3e25 
-    Nat = punit.nplasma(NatREAL,OMEGA_0)
-    Nat = np.ones(SIZE)*Nat
 
-    # Defines the lenght which we allow the gas to be ionized
-    PLASMASTART = 50
-    PLASMASTOPP = 100
+    #EREALenv = punit.Ereal(E,OMEGA_0)
+    #IREAL = epsilon*c*EREALenv**2/2    # This is the real pulse!
+
+    mplot.plot(plotz,E)
+
+    PLASMASTART = PULSELENGTH+PULSESTART
+    PLASMASTOPP = SIZE
+    RAMPLENGTH = 600
+    RAMP_DAMP = 0.1
+
+
+    Natpunit = punit.nplasma(NatREAL,OMEGA_0)
+    Nat = np.ones(SIZE)*Natpunit
+
+    Rampfunctions.Ramp_exp(RAMPLENGTH,PLASMASTART,PLASMASTOPP,RAMP_DAMP,Natpunit,Nat,SIZE,dt)
+
+    mplot.plot(plotz,Nat)
+    Nkritisk = punit.nreal(1,OMEGA_0)
     
     print(str(datetime.now())+': Beginning simulation.')
     for i in range(1,TIME):
-
+    
         E = SpaceSolver.E(E,B,J,dt,dz)
-        B = SpaceSolver.B(E,B,dt,dz)
-
-        for z in range(len(E)):
-            if abs(E[z]) == 0:
-                W1[z] = 0
-                W2[z] = 0
-            else:
-                if z > PLASMASTART and z < PLASMASTOPP:
-                    W1[z] = Ion.Landau(E[z],OMEGA_0,1,dt)
-                    W2[z] = Ion.Landau(E[z],OMEGA_0,2,dt)
-                else:
-                    W1[z] = 0
-                    W2[z] = 0
-
-        for z in range(len(E)):
-            Ni0[z] = Nat[z]-Ni1[z]
-            Ni1[z] = (Ni1[z]*(1-(dt/2)*W2[z])+(dt/2)*W1[z]*(Ni0[z]+Ni0temp[z]))/(1+(dt/2)*W2[z])
-            ne[z] = 1*Ni1[z]
-            Ni0temp[z] = Ni0[z]
-
+        B = SpaceSolver.B(E,B,dt,dz)   
+        ne = SpaceSolver.N(E,Nat,Ni0,Ni1,Ni2,Ni0temp,Ni1temp,ne,W1,W2,W3,OMEGA_0,dt)
+        Ni0temp = Ni0
+        Ni1temp = Ni1
         J = SpaceSolver.J(E,J,ne,ntemp,nu,dt,dz)
         netemp = ne
 
+
         Etot[i] = E
         Jtot[i] = J
-        W1tot[i] = W1
         netot[i] = ne
-        Ni0tot[i] = Ni0
-        Ni1tot[i] = Ni1
     print(str(datetime.now())+': Simulation complete.')
     z = np.arange(len(Etot[0]))
     plotnsave(z, Etot[100], '', 'etot.png')
