55b58c503b2f6c9cd9996073a6d8fea47648e401
erikadamstrandberg <erikadamstrandberg@gmail.com>
2018-03-02 17:00:30 +0100

    Update Rampfunctions.py

--- Rampfunctions.py
+++ Rampfunctions.py
@@ -1,13 +1,15 @@
 """ Creates a ramp for the atom density to make the current less diskont.    """
-""" The exponential ramp scales with the SIZE of the simulation window       """
-""" With the linear ramp you have to scale the RAMPLENGTH with the SIZE      """
-
+""" The exponential ramp scales with the SIZE of the simulation window.      """
+""" Linear Erik lets you choose the RAMPLENGTH and adjusts the scaling.      """
+""" Linear Hanna lets you choose the RAMP_FRAC for how big of a fraction of  """
+""" the atom density to be a ramp.                                           """
+  
 #%%
 import numpy as np
 
 def Ramp_exp(PLASMASTART,PLASMASTOPP,RAMP_DAMP,Natpunit,Nat,SIZE,dz):
-    x = np.array(SIZE)
-    i = np.arange(0,SIZE*dz,dz)
+    x = np.array(PLASMASTOPP)
+    i = np.arange(0,PLASMASTOPP*dz,dz)
     x = 1-np.exp(-i*RAMP_DAMP)
     
     for z in range(SIZE):
@@ -15,16 +17,60 @@ def Ramp_exp(PLASMASTART,PLASMASTOPP,RAMP_DAMP,Natpunit,Nat,SIZE,dz):
             Nat[z] = x[z-PLASMASTART]*Natpunit
         else:
             Nat[z] = 0
+    if SIZE > PLASMASTOPP:
+        xback = np.flipud(x)
+        for z in range(SIZE):
+            if z > PLASMASTART and z < PLASMASTOPP:
+                Nat[z] = xback[z]*Nat[z]
+            else:
+                Nat[z] = 0
+        
             
-def Ramp_linear(RAMPLENGTH,PLASMASTART,PLASMASTOPP,Natpunit,Nat,SIZE,dz):
+def Ramp_linear_Erik(PLASMASTART,PLASMASTOPP,RAMPLENGTH,Natpunit,Nat,SIZE,dz):
     x = np.arange(0,RAMPLENGTH*dz,dz)
     xnorm = x/(RAMPLENGTH*dz)
+    xnormback = np.flipud(xnorm)
+    MITTEN = (PLASMASTART + PLASMASTOPP)/2
     
     for z in range(SIZE):
-        if z > PLASMASTART and z < PLASMASTOPP:
+        
+        if z > PLASMASTART and z < PLASMASTOPP:  
             if z < PLASMASTART + RAMPLENGTH:
                 Nat[z] = xnorm[z-PLASMASTART]*Natpunit
             else:
-                Nat[z] = 1*Natpunit
+                Nat[z] = 1*Natpunit   
+            if PLASMASTOPP < SIZE:
+                if (PLASMASTOPP - RAMPLENGTH) < (PLASMASTART + RAMPLENGTH):
+                    if z > MITTEN:
+                        Nat[z] = xnormback[z-PLASMASTOPP]*Natpunit
+                else:
+                    if z > PLASMASTOPP - RAMPLENGTH and z < PLASMASTOPP:
+                        Nat[z] = xnormback[z-PLASMASTOPP]*Natpunit
         else:
-                Nat[z] = 0
+            Nat[z] = 0
+ 
+           
+def Ramp_linear_Hanna(PLASMASTART,PLASMASTOPP,RAMP_FRAC,Natpunit,Nat,SIZE,dz):
+    L = PLASMASTOPP-PLASMASTART
+    RAMPLENGTH = RAMP_FRAC*L
+    x = np.arange(0,RAMPLENGTH*dz,dz)
+    xnorm = x/(RAMPLENGTH*dz)
+    xnormback = np.flipud(xnorm)
+    MITTEN = (PLASMASTART + PLASMASTOPP)/2
+    
+    for z in range(SIZE):
+        
+        if z > PLASMASTART and z < PLASMASTOPP:  
+            if z < PLASMASTART + RAMPLENGTH:
+                Nat[z] = xnorm[z-PLASMASTART]*Natpunit
+            else:
+                Nat[z] = 1*Natpunit   
+            if PLASMASTOPP < SIZE:
+                if (PLASMASTOPP - RAMPLENGTH) < (PLASMASTART + RAMPLENGTH):
+                    if z > MITTEN:
+                        Nat[z] = xnormback[z-PLASMASTOPP]*Natpunit
+                else:
+                    if z > PLASMASTOPP - RAMPLENGTH and z < PLASMASTOPP:
+                        Nat[z] = xnormback[z-PLASMASTOPP]*Natpunit
+        else:
+            Nat[z] = 0
