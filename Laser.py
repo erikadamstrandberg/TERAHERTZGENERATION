003ed13bbb7d2c492fee7f0a537e71ac5b50eb07
erikadamstrandberg <erikadamstrandberg@gmail.com>
2018-03-08 13:52:24 +0100

    Update Laser.py

--- Laser.py
+++ Laser.py
@@ -28,3 +28,16 @@ def Gauss_backward(E,B,E0,PULSELENGTH,PULSESTART,OMEGAPRIM,t0,dt):
         Bl[i] = -E0*(np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*t0**2))))
     E[PULSESTART:PULSESTART+PULSELENGTH] = El
     B[PULSESTART:PULSESTART+PULSELENGTH] = Bl
+   
+def TwoC_forward(E,B,E0,xi,phi,PULSELENGTH,PULSESTART,OMEGAPRIM,t0,dt):
+    El = np.zeros(PULSELENGTH)
+    Bl = np.zeros(PULSELENGTH)
+    START = -PULSELENGTH/2  
+    STOPP = PULSELENGTH/2
+    t = dt*np.arange(START,STOPP,1)
+    for i in range(len(El)):
+        El[i] = E0*(np.sqrt(1-xi)*np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*t0**2)))+np.sqrt(xi)*np.cos(2*OMEGAPRIM*t[i]+phi)*np.exp(-4*(t[i]**2/(2*t0**2))))
+    for i in range(len(Bl)):
+        Bl[i] = E0*(np.sqrt(1-xi)*np.cos(OMEGAPRIM*t[i])*np.exp(-(t[i]**2/(2*t0**2)))+np.sqrt(xi)*np.cos(2*OMEGAPRIM*t[i]+phi)*np.exp(-4*(t[i]**2/(2*t0**2))))
+    E[PULSESTART:PULSESTART+PULSELENGTH] = El
+    B[PULSESTART:PULSESTART+PULSELENGTH] = Bl
