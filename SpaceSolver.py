b0e5022f1bc393b8147411146d1a99998bec1acb
erikadamstrandberg <erikadamstrandberg@gmail.com>
2018-03-27 16:23:45 +0200

    Update SpaceSolver.py

--- SpaceSolver.py
+++ SpaceSolver.py
@@ -14,38 +14,33 @@ def B(E,B,dt,dz):
     return B
 
 def J(E,J,ne,nu,dt,dz):
-    return J + dt*ne[0,:]*E
-    #dt2 = dt/2
-    #for z in range(len(J)):
         
-        # Update eq. for the J-field.
-        #J[z] = ((1-nu*dt2)*J[z]+(dt2)*(ne[0][z]+ne[1][z])*E[z])/(1+nu*dt2)
-        
-        # Makes temp. electron density need for the next time step.
-        #ne[1][z] = ne[0][z]
+    # Update eq. for the J-field.
+    J = ((1-nu*dt/2)*J+(dt/2)*(ne[0]+ne[1])*E)/(1+nu*dt/2)
+    
+    # Makes temp. electron density need for the next time step.
+    ne[1] = ne[0]
+    return J
 
 def N(E,Nat,Ni0,Ni1,Ni2,ne,W1,W2,W3,OMEGA_0,dt):
     
-    dt2 = dt/2
-    for z in range(len(E)):
-        
-        # Calculates all the needed ionization propabilites.
-        W1[z] = Ion.Landau_element(E[z],OMEGA_0,1,dt)
-        W2[z] = Ion.Landau_element(E[z],OMEGA_0,2,dt)
-        W3[z] = Ion.Landau_element(E[z],OMEGA_0,3,dt)
-        
-        # How many neutral atoms are left?
-        Ni0[0][z] = Nat[z]-Ni1[0][z]-Ni2[0][z]
-        
-        # Update eq. for the ionization density.
-        Ni1[0][z] = (Ni1[0][z]*(1-(dt2)*W2[z])+(dt2)*W1[z]*(Ni0[0][z]+Ni0[1][z]))/(1+(dt2)*W2[z])
-        Ni2[0][z] = (Ni2[0][z]*(1-(dt2)*W3[z])+(dt2)*W2[z]*(Ni1[0][z]+Ni1[1][z]))/(1+(dt2)*W3[z])
-        
-        # Updates the new electron density.
-        ne[0][z] = 1*Ni1[0][z] + 2*Ni2[0][z]
-        
-        # Makes temp. densities needed for the calculation in the next time step.
-        Ni0[1][z] = Ni0[0][z]
-        Ni1[1][z] = Ni1[0][z]
-        Ni2[1][z] = Ni2[0][z]
+    # Calculates all the needed ionization propabilites.
+    W1 = Ion.Landau_array(E,W1,OMEGA_0,1,dt)
+    W2 = Ion.Landau_array(E,W2,OMEGA_0,2,dt)
+    W3 = Ion.Landau_array(E,W2,OMEGA_0,3,dt)
+    
+    # How many neutral atoms are left?
+    Ni0[0] = Nat-Ni1[0]-Ni2[0]
+    
+    # Update eq. for the ionization density.
+    Ni1[0] = (Ni1[0]*(1-(dt/2)*W2)+(dt/2)*W1*(Ni0[0]+Ni0[1]))/(1+(dt/2)*W2)
+    Ni2[0] = (Ni2[0]*(1-(dt/2)*W3)+(dt/2)*W2*(Ni1[0]+Ni1[1]))/(1+(dt/2)*W3)
+    
+    # Updates the new electron density.
+    ne[0] = 1*Ni1[0] + 2*Ni2[0]
+    
+    # Makes temp. densities needed for the calculation in the next time step.
+    Ni0[1] = Ni0[0]
+    Ni1[1] = Ni1[0]
+    Ni2[1] = Ni2[0]
     return ne
