3be5ff9063b1e0d05becfe168dd07cf5e7c04aed
CooolLaserPlasma <36275582+CooolLaserPlasma@users.noreply.github.com>
2018-04-15 14:12:44 +0200

    Update Laser.py

--- Laser.py
+++ Laser.py
@@ -44,3 +44,17 @@ def TwoC_forward(E,B,E0,xi,phi,PULSELENGTH,PULSESTART,OMEGAPRIM,t0,dt,dz):
         Bl[i] = E0*(np.sqrt(1-xi)*np.sin(OMEGAPRIM*t[i])+np.sqrt(xi)*np.sin(2*OMEGAPRIM*t[i]+phi))*np.exp(-(t[i]**2/(2*t0**2)))
     E[PULSESTART:PULSESTART+PULSELENGTH] = El
     B[PULSESTART:PULSESTART+PULSELENGTH] = Bl
+    
+    def Super_Pulse(E,B,E0,PULSELENGTH,PULSESTART,OMEGAPRIM,t0,dt,dz):
+    El = np.zeros(PULSELENGTH)
+    Bl = np.zeros(PULSELENGTH)
+    START = -PULSELENGTH/2  
+    STOPP = PULSELENGTH/2
+    t = dz*np.arange(START,STOPP,1)
+    for i in range(len(El)):
+        El[i] = E0*(np.cos(OMEGAPRIM*t[i])-(2/t0**2)*t[i]*np.sin(OMEGAPRIM*t[i]))*np.exp(-(t[i]/t0)**2)
+    t = dz*np.arange(START,STOPP,1)+(dz-dt)/2
+    for i in range(len(Bl)):
+        Bl[i] = E0*(np.cos(OMEGAPRIM*t[i])-(2/t0**2)*t[i]*np.sin(OMEGAPRIM*t[i]))*np.exp(-(t[i]/t0)**2)
+    E[PULSESTART:PULSESTART+PULSELENGTH] = El
+    B[PULSESTART:PULSESTART+PULSELENGTH] = Bl
